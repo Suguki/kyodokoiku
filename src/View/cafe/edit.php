700621e2a92d099d3331315e785f5efd59fc0a22
suguki <suguki.cr@gmail.com>
2020-08-14 12:55:57 +0900

    編集画面に戻るボタンを追加。

--- src/View/cafe/edit.php
+++ src/View/cafe/edit.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset = "UTF-8">
         <title>今日どこいく？</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="../../style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -14,9 +14,11 @@
     <body>
         <?php foreach ($results as $result) { ?>
             <div class="input-container m1">
+            <div class="card">
+                <a href="../manager/index.php" class="btn btn-success position">戻る</a>
                 <form action="../../cafe/update.php" method="post">
-                    <div class="option">どのエリア？</div>
                     <h3><?php echo $result['place']; ?></h3>
+                    <div class="option">どのエリア？</div>
                         <select name="prefecture" >
                             <option value="大阪"
                             <?php if (isset($prefecture)) { ?>
@@ -123,10 +125,12 @@
                      </div>
                 </form>
                 <div class="title">
-                        <form action="../../manager/editCafe.php" name="delete">
-                          <button type="submit">削除</button>
-                        </form>
+                    <form action="../../manager/editCafe.php" name="delete">
+                      <button type="submit">削除</button>
+                    </form>
                 </div>
+            </div>
+ 
             </div>
          <?php } ?>
     </body>
--- src/View/food/edit.php
+++ src/View/food/edit.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang = "ja">
     <head>
-        <meta charaset = "UTF-8">
+        <meta charset = "UTF-8">
         <title>今日どこいく？</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="../../style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
-
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </head>
     <body>
         <?php foreach ($results as $result) { ?>
             <div class="input-container m1">
+                <div class="card">
+                <a href="../manager/index.php" class="btn btn-success position">戻る</a>
                 <form action="../../food/update.php" method="post">
                     <h3><?php echo $result['place']; ?></h3>
                     <p>遠くへ行きたい？近場がいい？</p>
@@ -87,6 +88,8 @@
                           <button type="submit">削除</button>
                         </form>
                 </div>
+                </div>
+
             </div>
          <?php } ?>
     </body>
