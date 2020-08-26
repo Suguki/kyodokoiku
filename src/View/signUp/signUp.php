e807aef1d6cd796301e75ebb010a2d9786948ffb
suguki <suguki.cr@gmail.com>
2020-08-26 09:35:57 +0900

    戻るボタンのリンク先を修正。

--- src/View/signUp/signUp.php
+++ src/View/signUp/signUp.php
@@ -22,6 +22,6 @@
                 <h1><?php echo $errorMessage?></h1>
             <?php } ?>
         <?php } ?>
-        <a class="btn-square" href="../top/">戻る</a>
+        <a class="btn btn-success" href="../../Top/top.php">戻る</a>
     </body>
 </html>
